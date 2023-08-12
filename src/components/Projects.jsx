f2508e65dabad94d69240a2a9bc907dfd44c2b0f
Robertw8 <oniedgaf@gmail.com>
2023-08-12 17:25:42 +0300

    make projects responsible

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,7 @@ export const Projects = () => {
 							/>
 						</div>
 						<div className='project-info'>
-							<h3 className='project-title'>"The Watch Spot" Landing page</h3>
+							<h3 className='project-title'>Team Project: "The Watch Spot" Landing page</h3>
 							<ul className='technologies-list'>
 								<li className='technologies-item'>HTML/CSS</li>
 								<li className='technologies-item'>JavaScript</li>
@@ -27,6 +27,30 @@ export const Projects = () => {
 							<div className='project-links-wrapper'>
 								<Link className='project-link' text='View Project' href='https://gladja.github.io/team-project/' />
 								<Link className='project-link' text='View Code' href='https://github.com/Robertw8/the-watch-spot' />
+							</div>
+						</div>
+					</li>
+					<li className='projects-item'>
+						<div className='image-thumb'>
+							<img
+								src='src/assets/images/projects/images.jpg'
+								alt='Images Search preview'
+								loading='lazy'
+								width='350'
+								height='250'
+								className='project-image'
+							/>
+						</div>
+						<div className='project-info'>
+							<h3 className='project-title'>Images Search using Pixabay API</h3>
+							<ul className='technologies-list'>
+								<li className='technologies-item'>HTML/CSS</li>
+								<li className='technologies-item'>JavaScript</li>
+								<li className='technologies-item'>REST API</li>
+							</ul>
+							<div className='project-links-wrapper'>
+								<Link className='project-link' text='View Project' href='https://robertw8.github.io/goit-js-hw-11/' />
+								<Link className='project-link' text='View Code' href='https://github.com/Robertw8/goit-js-hw-11' />
 							</div>
 						</div>
 					</li>
@@ -42,14 +66,14 @@ export const Projects = () => {
 							/>
 						</div>
 						<div className='project-info'>
-							<h3 className='project-title'>Web-Studio Landing page</h3>
+							<h3 className='project-title'>The First Project: WebStudio Landing page</h3>
 							<ul className='technologies-list'>
 								<li className='technologies-item'>HTML/CSS</li>
 								<li className='technologies-item'>JavaScript</li>
 							</ul>
 							<div className='project-links-wrapper'>
 								<Link className='project-link' text='View Project' href='https://robertw8.github.io/web-studio/' />
-								<Link className='project-link' text='View Project' href='https://github.com/Robertw8/web-studio' />
+								<Link className='project-link' text='View Code' href='https://github.com/Robertw8/web-studio' />
 							</div>
 						</div>
 					</li>

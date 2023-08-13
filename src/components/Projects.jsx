dbf066ec3fbba66aec701a0ed23c47f24b476b39
Robertw8 <oniedgaf@gmail.com>
2023-08-13 21:06:17 +0300

    fix icons and fonts paths

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,21 +6,21 @@ export const Projects = () => {
 	const projects = [
 		{
 			title: 'Team Project: "The Watch Spot" Landing page',
-			imageSrc: "src/assets/images/projects/spot.jpg",
+			imageSrc: "./src/assets/images/projects/spot.jpg",
 			technologies: ["HTML/CSS", "JavaScript"],
 			projectLink: "https://gladja.github.io/team-project/",
 			codeLink: "https://github.com/Robertw8/the-watch-spot",
 		},
 		{
 			title: "Images Search using Pixabay API",
-			imageSrc: "src/assets/images/projects/images.jpg",
+			imageSrc: "./src/assets/images/projects/images.jpg",
 			technologies: ["HTML/CSS", "JavaScript", "REST API"],
 			projectLink: "https://robertw8.github.io/goit-js-hw-11/",
 			codeLink: "https://github.com/Robertw8/goit-js-hw-11",
 		},
 		{
 			title: "The First Project: WebStudio Landing page",
-			imageSrc: "src/assets/images/projects/web-studio.jpg",
+			imageSrc: "./src/assets/images/projects/web-studio.jpg",
 			technologies: ["HTML/CSS", "JavaScript"],
 			projectLink: "https://robertw8.github.io/web-studio/",
 			codeLink: "https://github.com/Robertw8/web-studio",

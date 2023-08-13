50794cdfa91ac8681ca44a3f8d3d1a76d190284e
Robertw8 <oniedgaf@gmail.com>
2023-08-13 05:27:22 +0300

    add navbar to header

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "./Link";
+
+export const NavBar = () => {
+	return (
+		<nav className='nav-bar'>
+			<ul className='nav-list'>
+				<li className='nav-item'>
+					<Link href='#skills' className='nav-link' text='Skills' />
+				</li>
+				<li className='nav-item'>
+					<Link href='#projects' className='nav-link' text='Projects' />
+				</li>
+				<li className='nav-item'>
+					<Link href='#contact' className='nav-link' text='Contact' />
+				</li>
+			</ul>
+		</nav>
+	);
+};

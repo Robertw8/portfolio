5d2694303d96632bcf7aabf2f169bea89497f5e7
Robertw8 <oniedgaf@gmail.com>
2023-08-13 20:30:16 +0300

    refactored projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,82 +1,41 @@
 import React from "react";
 import { Link } from "./Link";
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
+	const projects = [
+		{
+			title: 'Team Project: "The Watch Spot" Landing page',
+			imageSrc: "src/assets/images/projects/spot.jpg", // Змініть шлях до зображення
+			technologies: ["HTML/CSS", "JavaScript"],
+			projectLink: "https://gladja.github.io/team-project/",
+			codeLink: "https://github.com/Robertw8/the-watch-spot",
+		},
+		{
+			title: "Images Search using Pixabay API",
+			imageSrc: "src/assets/images/projects/images.jpg", // Змініть шлях до зображення
+			technologies: ["HTML/CSS", "JavaScript", "REST API"],
+			projectLink: "https://robertw8.github.io/goit-js-hw-11/",
+			codeLink: "https://github.com/Robertw8/goit-js-hw-11",
+		},
+		{
+			title: "The First Project: WebStudio Landing page",
+			imageSrc: "src/assets/images/projects/web-studio.jpg", // Змініть шлях до зображення
+			technologies: ["HTML/CSS", "JavaScript"],
+			projectLink: "https://robertw8.github.io/web-studio/",
+			codeLink: "https://github.com/Robertw8/web-studio",
+		},
+		// Додайте інші проекти тут
+	];
+
 	return (
 		<section className='section projects' id='projects'>
 			<div className='container projects-container'>
 				<h2 className='section-title projects-title'>Projects</h2>
 				<ul className='projects-list'>
-					<li className='projects-item'>
-						<div className='image-thumb'>
-							<img
-								src='src/assets/images/projects/spot.jpg'
-								alt='The Watch Spot preview'
-								className='project-image'
-								width='350'
-								height='250'
-								loading='lazy'
-							/>
-						</div>
-						<div className='project-info'>
-							<h3 className='project-title'>Team Project: "The Watch Spot" Landing page</h3>
-							<ul className='technologies-list'>
-								<li className='technologies-item'>HTML/CSS</li>
-								<li className='technologies-item'>JavaScript</li>
-							</ul>
-							<div className='project-links-wrapper'>
-								<Link className='project-link' text='View Project' href='https://gladja.github.io/team-project/' />
-								<Link className='project-link' text='View Code' href='https://github.com/Robertw8/the-watch-spot' />
-							</div>
-						</div>
-					</li>
-					<li className='projects-item'>
-						<div className='image-thumb'>
-							<img
-								src='src/assets/images/projects/images.jpg'
-								alt='Images Search preview'
-								loading='lazy'
-								width='350'
-								height='250'
-								className='project-image'
-							/>
-						</div>
-						<div className='project-info'>
-							<h3 className='project-title'>Images Search using Pixabay API</h3>
-							<ul className='technologies-list'>
-								<li className='technologies-item'>HTML/CSS</li>
-								<li className='technologies-item'>JavaScript</li>
-								<li className='technologies-item'>REST API</li>
-							</ul>
-							<div className='project-links-wrapper'>
-								<Link className='project-link' text='View Project' href='https://robertw8.github.io/goit-js-hw-11/' />
-								<Link className='project-link' text='View Code' href='https://github.com/Robertw8/goit-js-hw-11' />
-							</div>
-						</div>
-					</li>
-					<li className='projects-item'>
-						<div className='image-thumb'>
-							<img
-								src='src/assets/images/projects/web-studio.jpg'
-								alt='Web-Studio preview'
-								loading='lazy'
-								width='350'
-								height='250'
-								className='project-image'
-							/>
-						</div>
-						<div className='project-info'>
-							<h3 className='project-title'>The First Project: WebStudio Landing page</h3>
-							<ul className='technologies-list'>
-								<li className='technologies-item'>HTML/CSS</li>
-								<li className='technologies-item'>JavaScript</li>
-							</ul>
-							<div className='project-links-wrapper'>
-								<Link className='project-link' text='View Project' href='https://robertw8.github.io/web-studio/' />
-								<Link className='project-link' text='View Code' href='https://github.com/Robertw8/web-studio' />
-							</div>
-						</div>
-					</li>
+					{projects.map((project, index) => (
+						<ProjectCard key={index} {...project} />
+					))}
 				</ul>
 			</div>
 		</section>

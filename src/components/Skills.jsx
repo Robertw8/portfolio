3187eb98bbaaabb8f52b5ba726eddf0a1808c7bb
Robertw8 <oniedgaf@gmail.com>
2023-08-13 05:27:16 +0300

    remake skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,20 +1,70 @@
 import React from "react";
-import Lottie from "react-lottie";
 
 export const Skills = () => {
 	return (
-		<section className='section skills'>
+		<section className='section skills' id='skills'>
 			<div className='container skills-container'>
 				<h2 className='section-title skills-title'>Skills</h2>
 				<ul className='skills-list'>
-					<li className='skills-item'>HTML/CSS</li>
-					<li className='skills-item'>SASS</li>
-					<li className='skills-item'>JavaScript</li>
-					<li className='skills-item'>React/Redux</li>
-					<li className='skills-item'>React Native</li>
-					<li className='skills-item'>Node.js</li>
-					<li className='skills-item'>Git</li>
-					<li className='skills-item'>Firebase</li>
+					<li className='skills-item'>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-html'></use>
+							</svg>
+						</span>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-css'></use>
+							</svg>
+						</span>
+					</li>
+					<li className='skills-item'>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-sass'></use>
+							</svg>
+						</span>
+					</li>
+					<li className='skills-item'>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-javascript'></use>
+							</svg>
+						</span>
+					</li>
+					<li className='skills-item'>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-react'></use>
+							</svg>
+						</span>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-redux'></use>
+							</svg>
+						</span>
+					</li>
+					<li className='skills-item'>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-node'></use>
+							</svg>
+						</span>
+					</li>
+					<li className='skills-item'>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-git'></use>
+							</svg>
+						</span>
+					</li>
+					<li className='skills-item'>
+						<span className='icon-container'>
+							<svg width='100%' height='100%' className='skills-icon'>
+								<use href='src/assets/images/sprite.svg#icon-firebase'></use>
+							</svg>
+						</span>
+					</li>
 				</ul>
 			</div>
 			<span className='skills-rings'>
@@ -28,6 +78,13 @@ export const Skills = () => {
 					</g>
 				</svg>
 			</span>
+			{/* <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+				<path
+					fill='#242424'
+					fillOpacity='1'
+					d='M0,288L60,266.7C120,245,240,203,360,197.3C480,192,600,224,720,197.3C840,171,960,85,1080,85.3C1200,85,1320,171,1380,213.3L1440,256L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z'
+				></path>
+			</svg> */}
 		</section>
 	);
 };

c342e355a39c51ccc1cce5f46d8b54de018bb127
Robertw8 <oniedgaf@gmail.com>
2023-08-14 19:05:58 +0300

    install and implement middleware for MIME types

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,16 @@ export default defineConfig({
 		exclude: ["js-big-decimal"],
 	},
 	base: "/portfolio/",
+	server: {
+		// ...
+		middleware: [
+			(req, res, next) => {
+				const mimeType = mime.lookup(req.url);
+				if (mimeType) {
+					res.setHeader("Content-Type", mimeType);
+				}
+				next();
+			},
+		],
+	},
 });

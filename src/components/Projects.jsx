a3f6f62e1b754e7cfcf765a5ad5db94164a36d83
Robertw8 <oniedgaf@gmail.com>
2023-08-30 16:48:41 +0300

    change project images paths

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,21 +10,21 @@ export const Projects = () => {
 	const projects = [
 		{
 			title: 'Team Project: "The Watch Spot" Landing page',
-			imageSrc: images,
+			imageSrc: spot,
 			technologies: ["HTML/CSS", "JavaScript"],
 			projectLink: "https://gladja.github.io/team-project/",
 			codeLink: "https://github.com/Robertw8/the-watch-spot",
 		},
 		{
 			title: "Team Project: Tasty Treats",
-			imageSrc: spot,
+			imageSrc: treats,
 			technologies: ["HTML", "SCSS", "JavaScript", "REST API"],
 			projectLink: "https://robertw8.github.io/project-recipes/",
 			codeLink: "https://github.com/Robertw8/project-recipes",
 		},
 		{
 			title: "Images Search using REST API",
-			imageSrc: treats,
+			imageSrc: images,
 			technologies: ["HTML/CSS", "JavaScript", "REST API"],
 			projectLink: "https://robertw8.github.io/goit-js-hw-11/",
 			codeLink: "https://github.com/Robertw8/goit-js-hw-11",

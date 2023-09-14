d8a25d59fa1a55d5d055a6e144464981cfa3e1be
Robertw8 <oniedgaf@gmail.com>
2023-09-14 03:14:26 +0300

    restyle Title

--- src/components/Title/Title.styled.js
+++ src/components/Title/Title.styled.js
@@ -1,11 +1,9 @@
 import styled from "styled-components";
 
 const TitlePrimary = styled.h2`
-	font-family: "Space Grotesk", sans-serif;
 	font-weight: 700;
 	font-size: 24px;
 	line-height: calc(32 / 24);
-	color: #ffffff;
 	text-align: center;
 
 	@media screen and (min-width: 768px) {

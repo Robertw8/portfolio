863197a1b1977532725d1e44d011da35e6d5325d
Robertw8 <oniedgaf@gmail.com>
2023-09-21 00:52:54 +0300

    update skillsData.js

--- src/components/Skills/skillsData.js
+++ src/components/Skills/skillsData.js
@@ -2,7 +2,27 @@ import sprite from "../../assets/images/sprite.svg";
 
 export const skills = [
 	{
-		name: "HTML/CSS",
+		name: "JavaScript",
+		icon: `${sprite}#icon-javascript`,
+		link: "https://developer.mozilla.org/en-US/docs/Web/javascript",
+	},
+	{
+		name: "React",
+		icon: `${sprite}#icon-react`,
+		link: "https://reactjs.org/",
+	},
+	{
+		name: "Redux",
+		icon: `${sprite}#icon-redux`,
+		link: "https://redux.js.org/",
+	},
+	{
+		name: "Material-UI",
+		icon: `${sprite}#icon-mui`,
+		link: "https://mui.com/",
+	},
+	{
+		name: "HTML",
 		icon: `${sprite}#icon-html`,
 		link: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics",
 	},
@@ -17,19 +37,14 @@ export const skills = [
 		link: "https://sass-lang.com/",
 	},
 	{
-		name: "JavaScript",
-		icon: `${sprite}#icon-javascript`,
-		link: "https://developer.mozilla.org/en-US/docs/Web/javascript",
+		name: "Vite",
+		icon: `${sprite}#icon-vite`,
+		link: "https://vitejs.dev/",
 	},
 	{
-		name: "React",
-		icon: `${sprite}#icon-react`,
-		link: "https://reactjs.org/",
-	},
-	{
-		name: "Redux",
-		icon: `${sprite}#icon-redux`,
-		link: "https://redux.js.org/",
+		name: "Webpack",
+		icon: `${sprite}#icon-webpack`,
+		link: "https://webpack.js.org/",
 	},
 	{
 		name: "Git",

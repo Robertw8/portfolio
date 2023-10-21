47db4c65b83b7b2aa858bd79c821ab3e8bcaa2ec
Robertw8 <oniedgaf@gmail.com>
2023-10-22 02:26:16 +0300

    add noreferrer rules to navlink

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -19,7 +19,7 @@ export const NavBar = () => {
 					<Link href='#contact' text='Contact' target='_self' />
 				</li>
 				<li>
-					<Link href='https://gold-mona-28.tiiny.site' text='CV' target='_blank' />
+					<Link href='https://gold-mona-28.tiiny.site' text='CV' target='_blank' rel='noreferrer nofollow noopener' />
 				</li>
 			</NavList>
 		</nav>
